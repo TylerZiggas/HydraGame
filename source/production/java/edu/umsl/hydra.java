8de73ca66ddac9a48425799e6a79c174086dc525
twzmmp <twzmmp@umsystem.edu>
2020-10-18 00:11:31 -0500

    Done, waiting for peer review (added comments)

--- source/production/java/edu/umsl/hydra.java
+++ source/production/java/edu/umsl/hydra.java
@@ -12,37 +12,37 @@ public class hydra extends hydragame {
     @Setter
     private int numOfTails;
 
-    public void hydraSetUp() {
+    public void hydraSetUp() { // Set up for the Hydra
         boolean retry;
         do {
             try {
                 retry = false;
                 Scanner input = new Scanner(System.in);
                 System.out.println("How many heads does the Hydra you see have?");
-                numOfHeads = input.nextInt();
+                numOfHeads = input.nextInt(); // Number of Heads input
                 System.out.println("And how many tails does it have?");
-                numOfTails = input.nextInt();
+                numOfTails = input.nextInt(); // Number of Tails input
             } catch (InputMismatchException ex) {
-                System.out.println("That is not a number!");
+                System.out.println("That is not a number!"); // Catch if a character is entered
                 retry = true;
             }
         } while (retry);
     }
 
-    public int currentHeads() {
+    public int currentHeads() { // For user to check on number of heads
         return numOfHeads;
     }
 
-    public int currentTails() {
+    public int currentTails() { // For user to check on number of tails
         return numOfTails;
     }
 
-    public void currentBoth() {
+    public void currentBoth() { // For printing both heads and tails together
         System.out.println("The number of heads is " + numOfHeads);
         System.out.println("The number of tails is " + numOfTails);
     }
 
-    public int minimumMoves() {
+    public int minimumMoves() { // Calculate lowest number of moves to defeat the hydra
         int minimumMoves = 0;
         int minTails = numOfTails;
         int minHeads = numOfHeads;
@@ -60,7 +60,7 @@ public class hydra extends hydragame {
                 minHeads = minHeads - 2;
                 minimumMoves++;
             }
-            if (minHeads == 1 && minTails == 0) {
+            if (minHeads == 1 && minTails == 0) { // If there is 1 head left you can't win, return -1 to show that
                 minimumMoves = -1;
                 break;
             }
@@ -68,54 +68,54 @@ public class hydra extends hydragame {
         return minimumMoves;
     }
 
-    public boolean checkHydra(int attackNumber) {
+    public boolean checkHydra(int attackNumber) { // Verifying the attack
         switch(attackNumber) {
             case 1:
-                if (numOfHeads < 1) {
+                if (numOfHeads < 1) { // Make sure there are enough heads
                     System.out.println("You tried to swing at a head, but the Hydra doesn't have any!");
                     return false;
                 }
                 break;
             case 2:
-                if (numOfTails < 1) {
+                if (numOfTails < 1) { // Make sure there are enough Tails
                     System.out.println("You tried to swing at a tail, but the Hydra currently has none!");
                     return false;
                 }
                 break;
             case 3:
-                if (numOfTails < 2) {
+                if (numOfTails < 2) { // Make sure there are enough Tails
                     System.out.println("You tried to swing at two of the Hydra's tails, but it doesn't even have two!");
                     return false;
                 }
                 break;
             case 4:
-                if (numOfHeads < 2) {
+                if (numOfHeads < 2) { // Make sure there are enough Heads
                     System.out.println("You tried to swing at two of the Hydra's heads, but it doesn't even have two!");
                     return false;
                 }
                 break;
             default:
-                System.out.println("What should I use against this monster?");
+                System.out.println("What should I use against this monster?"); // Just in case the user gets here there is text
                 break;
         }
         return true;
     }
 
-    public void attackHydra(int attackNumber) {
-        if (attackNumber == 1) {
+    public void attackHydra(int attackNumber) { // If attack is verified, continue
+        if (attackNumber == 1) { // Nothing happens to the Hydra in this scenario
             System.out.println("You have cut off a Hydra head, but a new head grew in its place!");
-        } else if (attackNumber == 2) {
+        } else if (attackNumber == 2) { // Cutting off a tail makes 2 grow
             numOfTails++;
             System.out.println("You have cut off a Hydra tail, but two new tails grew in its place!");
-        } else if (attackNumber == 3) {
+        } else if (attackNumber == 3) { // Cutting off 2 tails makes 1 head grow
             numOfTails = numOfTails - 2;
             numOfHeads++;
             System.out.println("You have cut off exactly two of the Hydra's tails, but a new head grew in their place!");
-        } else if (attackNumber == 4) {
+        } else if (attackNumber == 4) { // Cutting off 2 heads is optimal and subtracts 2 heads without adding anything
             numOfHeads= numOfHeads - 2;
             System.out.println("You have cut off exactly two of the Hydra's heads!");
         } else {
-            System.out.println("How should I attack this creature?");
+            System.out.println("How should I attack this creature?"); // Another backup scenario incase the user gets here
         }
     }
 }
--- source/production/java/edu/umsl/hydragame.java
+++ source/production/java/edu/umsl/hydragame.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class hydragame {
 
-    public static void menuSelect() {
+    public static void menuSelect() { // Menu select for moves, information, and giving up
         System.out.println("Knight PyPy, what move shall you use to attack the terrifying Hydra?");
         System.out.println("1) Cut off exactly ONE of the Hydra's Heads");
         System.out.println("2) Cut off exactly ONE of the Hydra's Tails");
@@ -17,7 +17,7 @@ public class hydragame {
         System.out.println("-1) Give up");
     }
 
-    public static int attackSelect(int attackNumber, hydra hydra) {
+    public static int attackSelect(int attackNumber, hydra hydra) { // Select attack against the Hydra
         boolean retry;
         do {
             do {
@@ -25,7 +25,7 @@ public class hydragame {
                     retry = false;
                     Scanner input = new Scanner(System.in);
                     attackNumber = input.nextInt();
-                    if (attackNumber == 5) {
+                    if (attackNumber == 5) { // Want to reset for options 5, 6, 7 so turn retry to true
                         hydra.currentBoth();
                         retry = true;
                     } else if (attackNumber == 6) {
@@ -34,16 +34,16 @@ public class hydragame {
                     } else if (attackNumber == 7) {
                         menuSelect();
                         retry = true;
-                    } else if (attackNumber == -1) {
+                    } else if (attackNumber == -1) { // Since the user gives up, simply leave here and go back to main
                         return -1;
                     }
                 } catch (InputMismatchException ex) {
-                    System.out.println("That is not one of your moves Knight PyPy!");
+                    System.out.println("That is not one of your moves Knight PyPy!"); // Make sure it is a number
                     retry = true;
                 }
             } while (retry);
             if (attackNumber <= 0 || attackNumber >= 8) {
-                System.out.println("You have no attacks of that number, try an attack you do know!");
+                System.out.println("You have no attacks of that number, try an attack you do know!"); // Tell user if the input is invalid
             }
         } while (attackNumber <= 0 || attackNumber >= 8);
         return attackNumber;
@@ -53,32 +53,32 @@ public class hydragame {
         System.out.println("Princess Perly has been kidnapped by a magical Hydra! Please save her Knight PyPy!");
         int attackNumber = 0;
         hydra hydra = new hydra();
-        hydra.hydraSetUp();
-        int tempHeads;
+        hydra.hydraSetUp(); // Set up the Hydra
+        int tempHeads; // Keep track of progress through heads and tails
         int tempTails;
         int numOfTurns = 0;
         do {
-            menuSelect();
-            attackNumber = attackSelect(attackNumber, hydra);
-            if (attackNumber == -1){
+            menuSelect(); // Go to the menu
+            attackNumber = attackSelect(attackNumber, hydra); // Pick the attack
+            if (attackNumber == -1){ // If the user has given up, also makes sure congratulations doesn't happen at end
                 tempHeads = hydra.currentHeads();
                 tempTails = hydra.currentTails();
                 System.out.println("You have given up, there is no way to help the princess now.");
                 break;
             }
-            boolean validTurn = hydra.checkHydra(attackNumber);
-            if (validTurn) {
+            boolean validTurn = hydra.checkHydra(attackNumber); // Check validity of attack
+            if (validTurn) { // Attack if valid
                 hydra.attackHydra(attackNumber);
                 numOfTurns++;
             }
             tempHeads = hydra.currentHeads();
             tempTails = hydra.currentTails();
-            if (tempHeads == 1 && tempTails == 0) {
+            if (tempHeads == 1 && tempTails == 0) { // Check to see if it is impossible to win now
                 System.out.println("There is no longer a way to defeat this horrible beast.");
                 break;
             }
         } while (tempHeads != 0 || tempTails != 0);
-        if (tempHeads == 0 && tempTails == 0) {
+        if (tempHeads == 0 && tempTails == 0) { // Congratulate the player if they succeeded
             System.out.println("Congratulations!  You have defeated the Hydra and saved Princess Perly!");
             System.out.println("It took you a total of " + numOfTurns + " turns!");
         }
